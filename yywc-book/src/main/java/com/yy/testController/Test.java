47a9b67384e6a35e1ae53ae4da2e3c80b36615bc
Mueoil <1395234017@qq.com>
2025-05-08 11:27:10 +0800

    测试分支

---
+++ yywc-book/src/main/java/com/yy/testController/Test.java
@@ -0,0 +1,11 @@
+package com.yy.testController;
+
+/**
+ * @Author: 秋有鱼
+ * @Date: 2025/5/8 10:38
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
